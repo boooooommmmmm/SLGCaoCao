04f78905d67088fd9802c4c0fce6c4a41444e6f2
Shuyan Hu <hushuyan88@gmail.com>
2020-06-25 21:16:10 +0800

    update scripts

---
+++ Assets/Scripts/Tools/SeparateAnimationFromFBX.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public class SeparateAnimationFromFBX : Editor
+{
+    [MenuItem("AnimationClip/GetFilteredtoAnim &1", true)]
+    static void CreateAnim()
+    {
+        string targetPath = Application.dataPath + "/AnimationClip";
+        if (!Directory.Exists(targetPath))
+        {
+            Directory.CreateDirectory(targetPath);
+        }
+        Object[] SelectionAsset = Selection.GetFiltered(typeof(Object), SelectionMode.Unfiltered);
+        Debug.Log(SelectionAsset.Length);
+        foreach (Object Asset in SelectionAsset)
+        {
+            AnimationClip newClip = new AnimationClip();
+            EditorUtility.CopySerialized(Asset, newClip);
+            AssetDatabase.CreateAsset(newClip, "Assets/AnimationClip/" + Asset.name + ".anim");
+        }
+        AssetDatabase.Refresh();
+    }
+}
