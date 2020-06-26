7197fcc9293d648b72693715c15a69bf912e3eed
Shuyan Hu <hushuyan88@gmail.com>
2020-06-26 17:11:33 +0800

    auto create animator controller

--- Assets/Scripts/Tools/SeparateAnimationFromFBX.cs
+++ Assets/Scripts/Tools/SeparateAnimationFromFBX.cs
@@ -3,32 +3,82 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 using System.IO;
+using UnityEditor.Animations;
 
 public class SeparateAnimationFromFBX
 {
     [MenuItem("GameObject/Tools/SeparateAnimations &1")]
     static void CreateAnim()
     {
-        Object[] selectionAsset = Selection.GetFiltered(typeof(Object), SelectionMode.Unfiltered);
-        
+        Object[] selectionAsset = Selection.GetFiltered(typeof(GameObject), SelectionMode.Unfiltered);
+
         foreach (Object asset in selectionAsset)
         {
-            Debug.Log("asset name: " + asset.name);
-
-            string assetName = selectionAsset[0].name;
+            string assetName = asset.name;
             string targetPath = Application.dataPath + "Art/Animations/" + assetName;
             if (!Directory.Exists(targetPath))
             {
-                Debug.Log("not exists");
                 Directory.CreateDirectory(targetPath);
             }
 
-            AnimationClip newClip = new AnimationClip();
-            EditorUtility.CopySerialized(asset, newClip);
-            AssetDatabase.CreateAsset(newClip, targetPath + "/" + asset.name + ".anim");
+            //cache animation clips
+            var animationClipDir = new Dictionary<string, AnimationClip>();
+            Object[] objects = AssetDatabase.LoadAllAssetsAtPath(AssetDatabase.GetAssetPath(asset.GetInstanceID()));
+            foreach (Object obj in objects)
+            {
+                AnimationClip clip = obj as AnimationClip;
+                if (clip != null && !clip.name.StartsWith("__preview"))
+                    animationClipDir.Add(clip.name, clip);
+            }
 
-            Debug.Log(string.Format("Generate animations success! {0}", assetName));
+            //save
+            foreach (AnimationClip ac in animationClipDir.Values)
+            {
+                AnimationClip newClip = new AnimationClip();
+                EditorUtility.CopySerialized(ac, newClip);
+
+                //if (ac.name.Equals("run") || ac.name.Equals("victory") || ac.name.Equals("stand"))
+                //    newClip = true;
+
+                AssetDatabase.CreateAsset(newClip, "Assets/Art/Animations/" + assetName + "/" + ac.name + ".anim");
+            }
+            AssetDatabase.Refresh();
+
+            //create animator controller
+            AnimatorOverrideController aoc = new AnimatorOverrideController();
+            aoc.runtimeAnimatorController = AssetDatabase.LoadAssetAtPath<AnimatorController>("Assets/Art/Animations/_base/CharacterBaseAnimatorController.controller");
+
+            AnimationClipOverrides clipOverrides = new AnimationClipOverrides(aoc.overridesCount);
+            clipOverrides["attack"] = animationClipDir["attack"];
+            clipOverrides["fight_be_hit"] = animationClipDir["fight_be_hit_0"];
+            clipOverrides["fight_death"] = animationClipDir["fight_death"];
+            clipOverrides["run"] = animationClipDir["run"];
+            clipOverrides["skill"] = animationClipDir["skill"];
+            clipOverrides["skill_l"] = animationClipDir["skill_l"];
+            clipOverrides["stand"] = animationClipDir["stand"];
+            clipOverrides["victory"] = animationClipDir["victory"];
+
+            aoc.ApplyOverrides(clipOverrides);
+
+            AssetDatabase.CreateAsset(aoc, "Assets/Art/Animations/" + assetName + "/" + assetName + "AC" + ".overrideController");
+            AssetDatabase.Refresh();
         }
-        AssetDatabase.Refresh();        
+        AssetDatabase.Refresh();
+    }
+
+    public class AnimationClipOverrides : List<KeyValuePair<AnimationClip, AnimationClip>>
+    {
+        public AnimationClipOverrides(int capacity) : base(capacity) { }
+
+        public AnimationClip this[string name]
+        {
+            get { return this.Find(x => x.Key.name.Equals(name)).Value; }
+            set
+            {
+                int index = this.FindIndex(x => x.Key.name.Equals(name));
+                if (index != -1)
+                    this[index] = new KeyValuePair<AnimationClip, AnimationClip>(this[index].Key, value);
+            }
+        }
     }
 }
