2e7fa7c092c31f0647e5b3c24952d53380005ef0
boooooommmmmm <hushuyan88@gmail.com>
2020-06-28 00:15:33 +0800

    upload today's work

---
+++ Assets/Scripts/Core/Unit/BaseUnit.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TbsFramework.Units;
+
+namespace Framework.Core
+{
+    public class BaseUnit : Unit
+    {
+
+        public Color LeadingColor;
+        public override void Initialize()
+        {
+            base.Initialize();
+            transform.localPosition -= new Vector3(0, 0, 1);
+            GetComponent<Renderer>().material.color = LeadingColor;
+        }
+        public override void MarkAsAttacking(Unit other)
+        {
+        }
+
+        public override void MarkAsDefending(Unit other)
+        {
+        }
+
+        public override void MarkAsDestroyed()
+        {
+        }
+
+        public override void MarkAsFinished()
+        {
+        }
+
+        public override void MarkAsFriendly()
+        {
+            GetComponent<Renderer>().material.color = LeadingColor + new Color(0.8f, 1, 0.8f);
+        }
+
+        public override void MarkAsReachableEnemy()
+        {
+            GetComponent<Renderer>().material.color = LeadingColor + Color.red;
+        }
+
+        public override void MarkAsSelected()
+        {
+            GetComponent<Renderer>().material.color = LeadingColor + Color.green;
+        }
+
+        public override void UnMark()
+        {
+            GetComponent<Renderer>().material.color = LeadingColor;
+        }
+    }
+}
---
+++ Assets/Shaders/Character.shader
@@ -0,0 +1,67 @@
+﻿Shader "Unlit/Character"
+{
+	Properties
+	{
+		_MainTex("Texture", 2D) = "white" {}
+		_Gray("Gray", Range(0,1)) = 1
+	}
+		SubShader
+		{
+			Tags { "RenderType" = "Opaque" }
+			LOD 100
+
+			Pass
+			{
+				CGPROGRAM
+				#pragma vertex vert
+				#pragma fragment frag
+				// make fog work
+				#pragma multi_compile_fog
+
+				#include "UnityCG.cginc"
+
+				uniform fixed _Gray;
+
+				struct appdata
+				{
+					float4 vertex : POSITION;
+					float2 uv : TEXCOORD0;
+				};
+
+				struct v2f
+				{
+					float2 uv : TEXCOORD0;
+					UNITY_FOG_COORDS(1)
+					float4 vertex : SV_POSITION;
+				};
+
+				sampler2D _MainTex;
+				float4 _MainTex_ST;
+
+				v2f vert(appdata v)
+				{
+					v2f o;
+					o.vertex = UnityObjectToClipPos(v.vertex);
+					o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+					UNITY_TRANSFER_FOG(o,o.vertex);
+					return o;
+				}
+
+				fixed4 frag(v2f i) : SV_Target
+				{
+					// sample the texture
+					fixed4 col = tex2D(_MainTex, i.uv);
+				// apply fog
+				UNITY_APPLY_FOG(i.fogCoord, col);
+
+				//set gray
+				fixed gray = 0.2125 * col.r + 0.7154 * col.g + 0.0721 * col.b;
+				fixed3 grayColor = fixed3(gray, gray, gray);
+				fixed3 finalColor = lerp(grayColor, col, _Gray);
+
+				return fixed4(finalColor, col.a);
+			}
+			ENDCG
+		}
+		}
+}
