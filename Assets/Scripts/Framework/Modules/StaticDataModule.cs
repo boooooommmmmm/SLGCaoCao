7ebcf25bb190cb63cdf54f6fead8aec7894c19f5
boooooommmmmm <hushuyan88@gmail.com>
2020-07-05 00:11:58 +0800

    ++

--- Assets/Scripts/Framework/Modules/StaticDataModule.cs
+++ Assets/Scripts/Framework/Modules/StaticDataModule.cs
@@ -4,82 +4,86 @@ using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using UnityEngine;
+using Framework.Core;
 
-namespace Framework.Data
+namespace Game.Data
 {
-    public class StaticDataModule : Core.ModuleBase
+    public class StaticDataModule : ModuleBase
     {
-        private string galProcess = "001";
-        private List<string> listGalProcess = new List<string>();
-        private List<Dictionary<string, string>> listGal = new List<Dictionary<string, string>>();
+        //static data name; index; value pair
+        private Dictionary<string, Dictionary<int, Dictionary<string, string>>> staticData = new Dictionary<string, Dictionary<int, Dictionary<string, string>>>();
+
 
         public sealed override void Start()
         {
-            //StreamReader inp_stm = new StreamReader("Design/Characters.csv", Encoding.UTF8);
-            //while (!inp_stm.EndOfStream)
-            //{
-            //    string inp_ln = inp_stm.ReadLine();
-            //    Debug.Log(inp_ln);
-            //}
-            //inp_stm.Close();
 
-            //load gal config
-            listGalProcess.Clear();
-            if (galProcess.Equals("001"))
+            StreamReader inp_stm = new StreamReader("Design/Characters.csv", Encoding.UTF8);
+            List<string> listCharacterData = new List<string>();
+            while (!inp_stm.EndOfStream)
             {
-                StreamReader inp_stm = new StreamReader("Design/Gal/TableGal001.csv", Encoding.UTF8);
-                while (!inp_stm.EndOfStream)
-                {
-                    string inp_ln = inp_stm.ReadLine();
-                    listGalProcess.Add(inp_ln);
-                }
-                inp_stm.Close();
+                string inp_ln = inp_stm.ReadLine();
+                listCharacterData.Add(inp_ln);
             }
+            inp_stm.Close();
+            parseData("Character", listCharacterData);
 
-            parseGalData();
+            //load gal config            
+            List<string> listGalProcess = new List<string>();
+            inp_stm = new StreamReader("Design/Gal/TableGal001.csv", Encoding.UTF8);
+            while (!inp_stm.EndOfStream)
+            {
+                string inp_ln = inp_stm.ReadLine();
+                listGalProcess.Add(inp_ln);
+            }
+            inp_stm.Close();
+            parseData("Gal001", listGalProcess);
+
         }
 
-        void parseGalData()
+        void parseData(string dataName, List<string> listRawData)
         {
-            listGal = new List<Dictionary<string, string>>();
-            string[] arrGalDataStructure = listGalProcess[0].Split(',');
-
-            for (int i = 1; i <= listGalProcess.Count - 1; i++)
+            if (staticData.ContainsKey(dataName))
             {
-                string[] _tempArr = listGalProcess[i].Split(',');
-                Dictionary<string, string> _dir = new Dictionary<string, string>();
+                Debug.LogError(string.Format("Static data module error! Data [{0}] already in memory! ", dataName));
+                return;
+            }
+
+            Dictionary<string, string> _valuePair = new Dictionary<string, string>();
+            Dictionary<int, Dictionary<string, string>> _finalData = new Dictionary<int, Dictionary<string, string>>();
+
+            string[] arrDataStructure = listRawData[0].Split(',');
+            for (int i = 1; i <= listRawData.Count - 1; i++)
+            {
+                string[] _tempArr = listRawData[i].Split(',');
+                _valuePair = new Dictionary<string, string>();
 
                 for (int i2 = 0; i2 < _tempArr.Length; i2++)
                 {
-                    _dir.Add(arrGalDataStructure[i2], _tempArr[i2]);
+                    _valuePair.Add(arrDataStructure[i2], _tempArr[i2]);
                 }
 
-                listGal.Add(_dir);
+                _finalData[int.Parse(_valuePair["ID"])] = _valuePair;
             }
         }
 
-        public List<Dictionary<string, string>> GetFirstGalData()
+        public Dictionary<int, Dictionary<string, string>> GetData(string dataName)
         {
-            return listGal;
+            return staticData[dataName];
         }
 
-        //void parseData()
-        //{
-        //    for (int i = 0; i < stringList.Count; i++)
-        //    {
-        //        string[] temp = stringList[i].Split(",");
-        //        for (int j = 0; j < temp.Length; j++)
-        //        {
-        //            temp[j] = temp[j].Trim();  //removed the blank spaces
-        //        }
-        //        parsedList.Add(temp);
-        //    }
-        //    //you should now have a list of arrays, ewach array can ba appied to the script that's on the Sprite
-        //    //you'll have to figure out a way to push the data the sprite
-        //    SpriteScript.Name = parsedList[0];
-        //    SpriteScript.country = parsedList[1];
-        //    SpriteScript.size = parsedList[2];
-        //    SpriteScript.population = parsedList[3];
-        //}
+        public Dictionary<string, string> GetDataByIndex(string dataName, int index)
+        {
+            return staticData[dataName][index];
+        }
+
+        public Record GetRecord()
+        {
+            return JsonUtility.FromJson<Record>("path");
+        }
+
+        public void SaveRecord()
+        {
+            
+        }
     }
 }
